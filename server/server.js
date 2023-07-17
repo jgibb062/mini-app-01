bd8176c31548fe60ba6c9fd066269ee41a68536d
jgibb062 <jgibb062@gmail.com>
2023-07-17 09:46:15 -0700

    server componnents added

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+const port = 3001
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Start Server
+app.listen(port, () => {
+  console.log(`App is running on ${port}.`)
+});
+
